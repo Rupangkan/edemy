830a622821ee7275a0e355c3152963c5d3fdea67
Rupangkan <every1isnotrupangkan@gmail.com>
2022-04-14 21:11:23 +0530

    Till logout

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">
+        Learning Management System
+      </h1>
+    </>
+  );
+};
+
+export default Index;
