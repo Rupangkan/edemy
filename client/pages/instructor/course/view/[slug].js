020f14b4f730c25cb2535dd921cc60c7e2b1dcd5
Rupangkan <every1isnotrupangkan@gmail.com>
2022-06-05 15:18:09 +0530

    Added course update feature

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -1,22 +1,32 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import InstructorRoute from "../../../../components/routes/InstructorRoute";
 import axios from "axios";
-import { Avatar, Tooltip, Button, Modal } from "antd";
-import { EditOutlined, CheckOutlined, UploadOutlined } from "@ant-design/icons";
-import ReactMarkdown from 'react-markdown'
+import { Avatar, Tooltip, Button, Modal, List } from "antd";
+import {
+    EditOutlined,
+    CheckOutlined,
+    UploadOutlined,
+    QuestionOutlined,
+    CloseOutlined,
+} from "@ant-design/icons";
+import ReactMarkdown from "react-markdown";
 import AddLessonForm from "../../../../components/forms/AddLessonForm";
+import { toast } from "react-toastify";
+import Item from "antd/lib/list/Item";
 
 const CourseView = () => {
-    const [course, setCourse] = useState({})
-    //lessons
-    const [visible, setVisible] = useState(false)
+    const [course, setCourse] = useState({});
+    // for lessons
+    const [visible, setVisible] = useState(false);
     const [values, setValues] = useState({
         title: "",
         content: "",
-        video: "",
-    })
-    const [uploading, setUploading] = useState(false)
+        video: {},
+    });
+    const [uploading, setUploading] = useState(false);
+    const [uploadButtonText, setUploadButtonText] = useState("Upload Video");
+    const [progress, setProgress] = useState(0);
 
     const router = useRouter();
     const { slug } = router.query;
@@ -30,10 +40,104 @@ const CourseView = () => {
         setCourse(data);
     };
 
-    const handleAddLesson = e => {
+    // FUNCTIONS FOR ADD LESSON
+    const handleAddLesson = async (e) => {
         e.preventDefault();
-        console.log(values)
-    }
+        // console.log(values);
+        try {
+            const { data } = await axios.post(
+                `/api/course/lesson/${slug}/${course.instructor._id}`,
+                values
+            );
+            console.log(data)
+            setValues({ ...values, title: "", content: "", video: {} });
+            setProgress(0);
+            setUploadButtonText("Upload video");
+            setVisible(false);
+            setCourse(data);
+            toast.success("Lesson added");
+        } catch (err) {
+            console.log(err);
+            toast.error("Lesson add failed");
+        }
+    };
+
+    const handleVideo = async (e) => {
+        try {
+            const file = e.target.files[0];
+            setUploadButtonText(file.name);
+            setUploading(true);
+
+            const videoData = new FormData();
+            videoData.append("video", file);
+            // save progress bar and send video as form data to backend
+            const { data } = await axios.post(
+                `/api/course/video-upload/${course.instructor._id}`,
+                videoData,
+                {
+                    onUploadProgress: (e) => {
+                        setProgress(Math.round((100 * e.loaded) / e.total));
+                    },
+                }
+            );
+            // once response is received
+            console.log(data);
+            setValues({ ...values, video: data });
+            setUploading(false);
+            toast.success("Video uploaded successfully")
+        } catch (err) {
+            console.log(err);
+            setUploading(false);
+            toast.error("Video upload failed");
+        }
+    };
+
+    const handleVideoRemove = async () => {
+        try {
+            setUploading(true);
+            const { data } = await axios.post(
+                `/api/course/video-remove/${course.instructor._id}`,
+                values.video
+            );
+            console.log(data);
+            setValues({ ...values, video: {} });
+            toast.success("Video removed successfully")
+            setUploading(false);
+            setUploadButtonText("Upload another video");
+        } catch (err) {
+            console.log(err);
+            setUploading(false);
+            toast.error("Video remove failed");
+        }
+    };
+
+    const handlePublish = async (e, courseId) => {
+        try {
+            let answer = window.confirm(
+                "Once you publsih your course, it will be live in the marketplace for users to enroll"
+            );
+            if (!answer) return;
+            const { data } = await axios.put(`/api/course/publish/${courseId}`);
+            setCourse(data);
+            toast("Congrats! Your course is live");
+        } catch (err) {
+            toast("Course publish failed. Try again");
+        }
+    };
+
+    const handleUnpublish = async (e, courseId) => {
+        try {
+            let answer = window.confirm(
+                "Once you unpublsih your course, it will no be available for users to enroll"
+            );
+            if (!answer) return;
+            const { data } = await axios.put(`/api/course/unpublish/${courseId}`);
+            setCourse(data);
+            toast("Your course is unpublished");
+        } catch (err) {
+            toast("Course publish failed. Try again");
+        }
+    };
 
     return (
         <InstructorRoute>
@@ -61,11 +165,33 @@ const CourseView = () => {
 
                                     <div className="d-flex pt-4">
                                         <Tooltip title="Edit">
-                                            <EditOutlined className="h5 pointer text-warning mr-4" />
+                                            <EditOutlined
+                                                onClick={() =>
+                                                    router.push(`/instructor/course/edit/${slug}`)
+                                                }
+                                                className="h5 pointer text-warning mr-4"
+                                            />
                                         </Tooltip>
-                                        <Tooltip title="Publish">
-                                            <CheckOutlined className="h5 pointer text-danger" />
-                                        </Tooltip>
+
+                                        {course.lessons && course.lessons.length < 5 ? (
+                                            <Tooltip title="Min 5 lessons required to publish">
+                                                <QuestionOutlined className="h5 pointer text-danger" />
+                                            </Tooltip>
+                                        ) : course.published ? (
+                                            <Tooltip title="Unpublish">
+                                                <CloseOutlined
+                                                    onClick={(e) => handleUnpublish(e, course._id)}
+                                                    className="h5 pointer text-danger"
+                                                />
+                                            </Tooltip>
+                                        ) : (
+                                            <Tooltip title="Publish">
+                                                <CheckOutlined
+                                                    onClick={(e) => handlePublish(e, course._id)}
+                                                    className="h5 pointer text-success"
+                                                />
+                                            </Tooltip>
+                                        )}
                                     </div>
                                 </div>
                             </div>
@@ -73,7 +199,7 @@ const CourseView = () => {
                         <hr />
                         <div className="row">
                             <div className="col">
-                                <ReactMarkdown children={course.description} />
+                                <ReactMarkdown source={course.description} />
                             </div>
                         </div>
                         <div className="row">
@@ -83,22 +209,52 @@ const CourseView = () => {
                                 type="primary"
                                 shape="round"
                                 icon={<UploadOutlined />}
+                                size="large"
                             >
                                 Add Lesson
                             </Button>
                         </div>
-                        <Modal title="+ Add Lesson"
+
+                        <br />
+
+                        <Modal
+                            title="+ Add Lesson"
                             centered
                             visible={visible}
                             onCancel={() => setVisible(false)}
                             footer={null}
                         >
-                            <AddLessonForm values={values}
+                            <AddLessonForm
+                                values={values}
                                 setValues={setValues}
                                 handleAddLesson={handleAddLesson}
                                 uploading={uploading}
+                                uploadButtonText={uploadButtonText}
+                                handleVideo={handleVideo}
+                                progress={progress}
+                                handleVideoRemove={handleVideoRemove}
                             />
                         </Modal>
+
+                        <div className="row pb-5">
+                            <div className="col lesson-list">
+                                <h4>
+                                    {course && course.lessons && course.lessons.length} Lessons
+                                </h4>
+                                <List
+                                    itemLayout="horizontal"
+                                    dataSource={course && course.lessons}
+                                    renderItem={(item, index) => (
+                                        <Item>
+                                            <Item.Meta
+                                                avatar={<Avatar>{index + 1}</Avatar>}
+                                                title={item.title}
+                                            ></Item.Meta>
+                                        </Item>
+                                    )}
+                                ></List>
+                            </div>
+                        </div>
                     </div>
                 )}
             </div>
--- server/routes/course.js
+++ server/routes/course.js
@@ -1,12 +1,12 @@
 import express from "express";
-
+import formidable from 'express-formidable'
 const router = express.Router();
 
 // middleware
 import { requireSignIn, isInstructor } from "../middleware";
 
 // controllers
-import { uploadImage, removeImage, create, read } from "../controllers/course";
+import { uploadImage, removeImage, create, read, uploadVideo, removeVideo, addLesson, update } from "../controllers/course";
 
 // image
 router.post("/course/upload-image", uploadImage);
@@ -14,5 +14,9 @@ router.post("/course/remove-image", removeImage);
 // // course
 router.post("/course", requireSignIn, isInstructor, create);
 router.get("/course/:slug", read);
+router.post('/course/video-upload/:instructorId', requireSignIn, formidable(), uploadVideo)
+router.post('/course/video-remove/:instructorId', requireSignIn, removeVideo)
+router.post('/course/lesson/:slug/:instructorId', requireSignIn, addLesson)
+router.put('/course/:slug', requireSignIn, update)
 
 module.exports = router;
